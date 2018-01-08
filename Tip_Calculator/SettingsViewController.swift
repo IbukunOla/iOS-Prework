ed321bc8e435463d8487a0cc058d25a018980707
MacOS <ibukun@MacOSs-iMac.local>
2018-01-08 11:34:48 -0800

    Updated Everyhting

---
+++ Tip_Calculator/SettingsViewController.swift
@@ -0,0 +1,82 @@
+//
+//  SettingsViewController.swift
+//  Tip_Calculator
+//
+//  Created by MacOS on 12/23/17.
+//  Copyright © 2017 MacOS. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var DTLabel: UILabel!
+    @IBOutlet weak var dsLabel: UILabel!
+    @IBOutlet weak var defaultTip: UITextField!
+    @IBOutlet weak var dsSwitch: UISwitch!
+    
+    let black = UIColor.black
+    let white = UIColor.white
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // init Load
+         self.title = "Tip Calculator"
+        let darkSkin = UserDefaults.standard.bool(forKey: "darkSkin")
+        changeSkin(darkSkin)
+        dsSwitch.setOn(darkSkin, animated: true)
+        let defTip = UserDefaults.standard.double(forKey: "defaultTip")
+        defaultTip.text = String(defTip)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //Change skin color if button is switched
+        let darkSkin = UserDefaults.standard.bool(forKey: "darkSkin")
+        changeSkin(darkSkin)
+        dsSwitch.setOn(darkSkin, animated: true)
+    }
+    
+    func changeSkin(_ shouldChange: Bool) {
+        if shouldChange {
+            view.backgroundColor = black
+            DTLabel.textColor = white
+            dsLabel.textColor = white
+            
+        }
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        //Tap outside field to update
+        view.endEditing(true)
+        //For saving default global
+    }
+     
+    @IBAction func eraseContents(_ sender: Any) {
+        defaultTip.text = ""
+    }
+    
+    @IBAction func onChange(_ sender: Any) {
+        let dTip = Double(defaultTip.text!) ?? 0
+        if dTip < 1 {
+            //Incase the user put decimal
+            UserDefaults.standard.set(dTip, forKey: "defaultTip")
+        } else {
+            UserDefaults.standard.set(dTip / 100, forKey: "defaultTip")
+        }
+    }
+    @IBAction func skinChange(_ sender: Any) {
+        //Save in defaults
+        UserDefaults.standard.set(dsSwitch.isOn, forKey: "darkSkin")
+        changeSkin(dsSwitch.isOn)
+    }
+    /*
+     // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Tip_Calculator/ViewController.swift
+++ Tip_Calculator/ViewController.swift
@@ -10,16 +10,99 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var fieldView: UIView!
+    @IBOutlet weak var totalTextLabel: UILabel!
+    @IBOutlet weak var tipTextLabel: UILabel!
+    
+    @IBOutlet weak var testLabel: UILabel!
+    
+    var tipPercentages = [0.18, 0.2, 0.25]
+    let black = UIColor.black
+    let white = UIColor.white
+    
+    let locale = NSLocale.autoupdatingCurrent
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // First load
+        //Load defaults
+        
+        //Skin
+        let darkSkin = UserDefaults.standard.bool(forKey: "darkSkin")
+        changeSkin(darkSkin)
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //Make text field automatically pop up
+        billField.becomeFirstResponder()
+        
+        //Tip
+        let defTip = UserDefaults.standard.double(forKey: "defaultTip")
+        //Display it
+        if defTip != 0.0 {
+            for (index, value) in tipPercentages.enumerated(){
+                tipPercentages[index] = defTip + (0.5 * Double(index))
+            }
+            //change segments
+            tipControl.removeAllSegments()
+            tipControl.insertSegment(withTitle: String(format: "%.0f%%", tipPercentages[0] * 100), at: 0, animated: true)
+            tipControl.insertSegment(withTitle: String(format: "%.0f%%", tipPercentages[1] * 100), at: 1, animated: true)
+            tipControl.insertSegment(withTitle: String(format: "%.0f%%", tipPercentages[2] * 100), at: 2, animated: true)
+            tipControl.selectedSegmentIndex = 0
+        }
+        
+        
+        //Dark Skin for UI
+        let darkSkin = UserDefaults.standard.bool(forKey: "darkSkin")
+        changeSkin(darkSkin)
+    }
+    
+    func changeSkin(_ shouldChange: Bool){
+        if shouldChange {
+            view.backgroundColor = black
+            fieldView.backgroundColor = white
+            billField.textColor = black
+            tipLabel.textColor = white
+            totalLabel.textColor = white
+            totalTextLabel.textColor = white
+            tipTextLabel.textColor = white
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        if sender.accessibilityLabel == "billTR" {
+            //If you touch anywhere inside the view for the field go to the billfield
+            billField.becomeFirstResponder()
+        } else {
+            view.endEditing(true)
+        }
     }
-
-
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        let currencySymbol = locale.currencySymbol
+        
+        let formatter = NumberFormatter()
+        formatter.allowsFloats = true
+        formatter.alwaysShowsDecimalSeparator = true
+        formatter.currencyDecimalSeparator = "."
+        formatter.currencyGroupingSeparator = ","
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = currencySymbol
+        
+        let tipText = formatter.string(for: tip) ?? "0"
+        let totalText = formatter.string(for: total) ?? "0."
+        
+        tipLabel.text = tipText
+        totalLabel.text = totalText
+    }
+    
 }
 
